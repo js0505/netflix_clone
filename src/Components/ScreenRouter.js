1999f5b258cbaa9b16663f9e36fc6b605239808c
jisu <shinjs0505@naver.com>
2021-07-08 22:06:39 +0900

    Error

--- src/Components/ScreenRouter.js
+++ src/Components/ScreenRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
 import Detail from '../Routes/DetailScreen';
-import Home from '../Routes/HomeScreen'
-import TV from '../Routes/TvScreen'
+import Home from '../Routes/HomeScreen';
+import Tv from '../Routes/TvScreen'
 
 
 const ScreenRouter = () => {
@@ -13,7 +13,7 @@ const ScreenRouter = () => {
             <Header />
             <Switch>
                 <Route path={'/'} exact component={Home} />
-                <Route path={'/tv'} exact component={TV} />
+                <Route path={'/tv'} exact component={Tv} />
                 <Route path={'/search'} exact component={Home} />
                 <Route path={'/movie/:id'} component={Detail} />
                 <Route path={'/tv/:id'} component={Detail} />
