5ef6ea0383d3dd9dad23fd89cd6a86e8e367f1c7
jisu <shinjs0505@naver.com>
2021-07-15 10:14:34 +0900

    수정

--- src/Routes/DetailScreen/DetailContainer.js
+++ src/Routes/DetailScreen/DetailContainer.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useLocation, useParams } from 'react-router-dom'
 import { movieAPI, tvAPI } from '../../API'
 import DetailPresenter from './DetailPresenter';
@@ -22,8 +22,12 @@ const DetailContainer = () => {
         location: {}
     });
 
-    const getData = useCallback( async () => {
-            // pathname 안에 /movie/가 있는지 확인.
+    
+
+    useEffect(() => {
+        
+        const getData = async () => {
+        // pathname 안에 /movie/가 있는지 확인.
         const [result, resultErr] = location.pathname.includes('/movie/')
             ? await movieAPI.movieDetail(id)
             : await tvAPI.tvDetail(id)
@@ -50,11 +54,10 @@ const DetailContainer = () => {
             loading: false,
             location
         })
-    },[id, location])
-
-    useEffect(() => {
+        }
+        
         getData()
-    }, [getData])
+    }, [id, location])
     
 
     return (
--- src/Routes/SearchScreen/SearchContainer.js
+++ src/Routes/SearchScreen/SearchContainer.js
@@ -5,6 +5,10 @@ import SearchPresenter from './SearchPresenter';
 const SearchContainer = () => {
 
     const [keyword, setKeyword] = useState("");
+    //다른 페이지와 다르게 검색 페이지는 폼 입력 후에 데이터가 로드 되므로
+    //기존처럼 객체로 넣어두면 로딩이 필요하기 전에 이미 로딩 페이지가 로드 되어있어 버린다.
+    // 그래서 데이터를 받아오는 작업이 시작되면 로딩 화면출력, 로드 끝나면 전환 을 위해서
+    // state로 로딩을 관리한다.
     const [loading, setLoading] = useState(false);
 
     const [results, setResults] = useState({
@@ -30,7 +34,7 @@ const SearchContainer = () => {
             movies,
             tvs,
             moviesErr,
-            tvsErr
+            tvsErr,
         })
         //로딩 완료
         setLoading(false)
